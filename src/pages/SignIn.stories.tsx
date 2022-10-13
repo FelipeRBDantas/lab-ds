5d39874368ac925f9641fa677d1df3c015d246a0
feliperbdantas <feliperbdantas@outlook.com>
2022-10-13 20:32:35 -0300

    [feat] - Storybook - Pages - Formulário

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { SignIn } from './SignIn';
+
+export default {
+    title: 'Pages/SignIn',
+    component: SignIn,
+    args: {},
+    argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {}
